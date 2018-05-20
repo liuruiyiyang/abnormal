dfff0acea64e033a1c6c844f728f7de5d4151064
liuruiyiyang <245422695@qq.com>
2018-05-20 22:04:41 +0800

    add manual

---
+++ lstm.py
@@ -0,0 +1,226 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import os
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Activation
+import matplotlib.pyplot as plt
+import warnings
+from keras.optimizers import Adam
+from utils import SplitKPIList
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
+warnings.filterwarnings("ignore") #Hide messy Numpy warnings
+shift_window = 1
+# 是否加载已经训练好的模型
+IS_LOAD_MODEL = False
+nodes=10
+batch_size=1
+timestep=1
+epochs=1
+
+output_path = './BasicLSTM_output'
+nb_epoch=1
+
+
+
+def scale_data(data, label):
+    X_scaler = MinMaxScaler(feature_range=(0, 1))
+
+    X = data.values
+
+    scaled_X = X_scaler.fit_transform(X)
+
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+
+    y_train = label.values
+
+    scaled_y = y_scaler.fit_transform(y_train)
+
+    return X_scaler, y_scaler, scaled_X, scaled_y
+
+def fit_lstm(X, y, model_file):
+    n_sample = X.shape[0]  # 样本个数
+    n_feat_dim = X.shape[1]  # 特征维度
+    print("X n_feat_dim=" + str(n_feat_dim))
+    print("X n_sample=" + str(n_sample))
+    n_label_dim = y.shape[1]
+
+    # shape: (样本个数, time step, 特征维度)
+    X = X.reshape(int(n_sample/timestep), timestep, n_feat_dim)
+
+    y = y.reshape(int(n_sample/timestep), n_label_dim)
+
+    print('y:\n')
+    print(y)
+
+    layer_2_units = 40
+    # 构建模型
+    model = Sequential()
+
+    model.add(LSTM(input_dim=n_feat_dim, output_dim=layer_2_units, return_sequences=True))
+    model.add(LSTM(
+        layer_2_units,
+        return_sequences=False))
+    model.add(Dense(n_label_dim))
+    opt = Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    model.add(Activation("sigmoid"))
+    model.compile(loss='mean_squared_error', optimizer=opt, metrics=['acc'])
+
+    print('开始训练...')
+    for i in range(nb_epoch):
+        print('已迭代{}次（共{}次） '.format(i + 1, nb_epoch))
+        model.fit(X, y, epochs=epochs, batch_size=batch_size,  validation_split=0.02)
+
+        model.reset_states()
+
+    # 在所有训练样本上运行一次，构建cell状态
+    model.predict(X, batch_size=batch_size)
+
+    # 保存模型
+    model.save(model_file)
+
+    return model
+
+
+def conv2Noneflag(number):
+    if number == 1:
+        return np.nan
+    else:
+        return 0
+
+
+def forecast_lstm(model, X):
+    n_sample = X.shape[0]
+    n_feat_dim = X.shape[1]
+    X = X.reshape(-1,  n_feat_dim)
+    y_pred = model.predict(X, batch_size=batch_size)
+    print('model.predict(X, batch_size=batch_size):\n')
+    print(model.predict(X, batch_size=batch_size))
+    print('y_pred:\n')
+    print(y_pred)
+    print('y_pred-----end-------\n')
+    return y_pred
+
+
+def get_lstm_diff(KPI_ID_name):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+# if __name__ == '__main__':
+
+    train_data_path = 'resources/train.csv'
+    test_data_path = 'resources/test.csv'
+    augment_data_path = 'resources/augment_data/'
+    test_augment_data_path = 'resources/test_augment_data/'
+    full_result_path = 'resources/result/prediction.csv'
+    split_result_path = 'resources/result_split/prediction.csv'
+    # output_path = 'resources/label_prediction_plot'
+    test_data_raw = pd.read_csv(test_data_path)
+    train_data_raw = pd.read_csv(train_data_path)
+
+    KPI_LIST, KPI_ID = SplitKPIList(train_data_raw)
+    KPI_LIST_test, KPI_ID_test = SplitKPIList(test_data_raw)
+
+    # KPI_ID_name = '07927a9a18fa19ae' # 07927a9a18fa19ae 76f4550c43334374 a5bf5d65261d859a
+
+    index = KPI_ID.index(KPI_ID_name)
+    train_X_raw = pd.DataFrame(KPI_LIST[index])
+    X_train_reserve = train_X_raw.copy()
+    X_train_reserve = pd.DataFrame(X_train_reserve)
+    # y_reserve = pd.DataFrame()
+    y_reserve = X_train_reserve['value']
+
+    y_reserve = pd.DataFrame(y_reserve)
+
+    y_reserve = y_reserve.shift(-1)
+    y_reserve = pd.DataFrame(y_reserve)
+
+    y_reserve.ffill(inplace=True)
+    y_reserve = pd.DataFrame(y_reserve)
+    # print("y_reserve:", y_reserve)
+    y_reserve = y_reserve.values
+    X_reserve = X_train_reserve['value'].values
+    test_index = KPI_ID_test.index(KPI_ID_name)
+    test_X_raw = pd.DataFrame(KPI_LIST_test[test_index])
+    # X_raw = pd.DataFrame(KPI_LIST[index])
+    X_test = test_X_raw.copy()
+    del X_test['KPI ID']
+    del X_test['timestamp']
+    label = KPI_LIST[index].copy()['label']
+    KPI_LIST[index]['label'] = KPI_LIST[index]['label'].map(conv2Noneflag)
+    KPI_LIST[index] = KPI_LIST[index].dropna(axis=0)
+    del KPI_LIST[index]['KPI ID']
+    del KPI_LIST[index]['label']
+    del KPI_LIST[index]['timestamp']
+    df = pd.DataFrame(KPI_LIST[index])
+    # print("df:\n", df)
+    df_y = df.shift(-1)
+    df_y.ffill(inplace=True)
+    # print("df_y:\n", df_y)
+
+    y_test = X_test.shift(-1)
+    y_test.ffill(inplace=True)
+
+    test_data = X_test.values
+
+    test_label = y_test.values
+
+    train_X_scaler, train_y_scaler, train_scaled_X, train_scaled_y = scale_data(df, df_y)
+
+    test_scaled_X = train_X_scaler.transform(test_data)
+
+    test_scaled_y = train_y_scaler.transform(test_label)
+
+
+    pre_item = 'value'
+    model_file = './BasicLSTM_output/'+KPI_ID_name+'_lstm_model.h5'
+    # 加载LSTM模型
+    if os.path.exists(model_file):
+        lstm_model = load_model(model_file)
+    else:
+        # 训练LSTM模型
+        lstm_model = fit_lstm(train_scaled_X, train_scaled_y, model_file)
+        # print('{}模型文件不存在'.format(model_file))
+        # exit(0)
+
+    test_diff_df = get_diff(test_data, test_scaled_y, test_scaled_X, lstm_model, train_y_scaler)
+
+    train_scaled_X = train_X_scaler.transform(X_reserve)
+
+    train_scaled_y = train_y_scaler.transform(y_reserve)
+    train_diff_df = get_diff(X_train_reserve.values, train_scaled_y, train_scaled_X, lstm_model, train_y_scaler)
+    return train_diff_df, test_diff_df
+    # print("pred_daily_df['diff'] :", pred_daily_df['lsmt_diff'] )
+
+    # pred_daily_df.to_csv(os.path.join(output_path, 'kdd_pred_daily_df.csv'))
+    # pred_daily_df.plot()
+    # plt.savefig(os.path.join(output_path, KPI_ID_name + '_lstm_pred_test_value.png'))
+
+    # plt.show()
+
+def get_diff(test_data, test_scaled_y, test_scaled_X, lstm_model, train_y_scaler):
+    test_data = pd.DataFrame(test_data)
+    test_dates = test_data.index.tolist()
+    pred_daily_df = pd.DataFrame(columns=['True Value', 'Pred Value'], index=test_dates)
+    pred_daily_df['True Value'] = pd.DataFrame(test_scaled_y)
+    # test_scaled_X = test_scaled_X.reshape(int(test_scaled_X.shape[0] / timestep), timestep, test_scaled_X.shape[1])
+    # print("test_scaled_X\n", test_scaled_X)
+    n_sample = test_scaled_X.shape[0]  # 样本个数
+    n_feat_dim = 1
+    test_scaled_X = test_scaled_X.reshape(int(n_sample / timestep), timestep, n_feat_dim)
+    # print("after reshape test_scaled_X\n", test_scaled_X)
+    pred_value = lstm_model.predict(test_scaled_X)
+    rescaled_y_pred = train_y_scaler.inverse_transform(pred_value)
+
+    # print("rescaled_y_pred:")
+    # print(rescaled_y_pred[:, 0])
+    # print(len(rescaled_y_pred[:, 0]))
+    for i in range(len(rescaled_y_pred[:, 0])):
+        pred_daily_df.loc[i, 'Pred Value'] = rescaled_y_pred[i, 0]
+
+    # pred_daily_df.plot()
+
+    pred_daily_df['test_diff'] = pred_daily_df['Pred Value'] - pred_daily_df['True Value']
+    return pred_daily_df['test_diff']
---
+++ merge_tsfresh_manual.py
@@ -0,0 +1,311 @@
+import numpy as np
+import pandas as pd
+import heapq
+import xgboost as xgb
+from sklearn import metrics
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score,roc_auc_score,precision_score, recall_score,f1_score,confusion_matrix
+from sklearn.cross_validation import train_test_split
+from xgboost import XGBClassifier
+from sklearn.model_selection import StratifiedKFold
+from utils import  ReadDatatime, SplitKPIList, plot_ts_label
+from lstm import get_lstm_diff
+import os
+
+# reproducibility
+seed = 321
+
+def split_data(x, y, split):
+    x_train_split = x[:int(len(x) * split)]
+    x_test_split = x[int(len(x) * split):]
+    y_train_split = y[:int(len(y) * split)]
+    y_test_split = y[int(len(y) * split):]
+    return x_train_split, x_test_split, y_train_split, y_test_split
+
+
+def padding_y(y_label, w):
+    prediction_ = y_label[:w]
+    predict_ = pd.DataFrame(np.array(prediction_))
+    prediction_ = y_label
+    predict_ = predict_.append(pd.DataFrame(np.array(prediction_)), ignore_index=True)
+    return predict_
+
+
+def padding_shift_y(y_label, w):
+    prediction_ = y_label[:int(w / 2)]
+    predict_ = pd.DataFrame(np.array(prediction_))
+    prediction_ = y_label
+    predict_ = predict_.append(pd.DataFrame(np.array(prediction_)), ignore_index=True)
+    prediction_ = y_label[(len(y_label) - int(w / 2)):]
+    predict_ = predict_.append(pd.DataFrame(np.array(prediction_)), ignore_index=True)
+    return predict_
+
+
+def get_result(ts_KPI_ID_test, ts_timestamp, predict):
+    ts_KPI_ID_test = ts_KPI_ID_test.reset_index(drop=True)
+    ts_timestamp = pd.DataFrame(ts_timestamp).reset_index(drop=True)
+    ts_timestamp.insert(loc=0, column='KPI ID', value=ts_KPI_ID_test)
+    ts_timestamp.insert(loc=2, column='predict', value=predict)
+    # print(ts_timestamp)
+    return ts_timestamp
+
+
+def save(result_path, ts_result):
+    if os.path.isfile(result_path):
+        with open(result_path, 'a') as file:
+            ts_result.to_csv(file, header=False, index=False)
+    else:
+        ts_result.to_csv(result_path, header=True, index=False)
+
+
+def is_near_zero(number):
+    if number < 0.1:
+        return 1
+    else:
+        return 0
+
+
+def is_zero(number):
+    if number == 0:
+        return 1
+    else:
+        return 0
+
+
+def boolean2int(number):
+    if number:
+        return 1
+    else:
+        return 0
+
+def get_manual_feature(ts_df):
+    raw_df = pd.DataFrame(ts_df)
+    raw_value = raw_df['value']
+    # print("raw_df['value']\n", raw_value)
+    is_zero_feature = raw_df['value'].map(is_zero)
+    # print("is_zero_feature\n", is_zero_feature)
+    is_near_zero_feature = raw_df['value'].map(is_near_zero)
+    # print("is_near_zero_feature\n", is_near_zero_feature)
+    diff_feature = raw_df['value'].diff().fillna(0)
+    # print("diff_feature\n", diff_feature)
+    mean = raw_df['value'].mean()
+    diff_minus_mean_feature = diff_feature.apply(lambda x: (x - mean))
+    value_minus_mean_feature = raw_df['value'].apply(lambda x: (x - mean))
+    abs_value_minus_mean_feature = raw_df['value'].apply(lambda x: abs(x - mean))
+    is_over_mean_feature = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 0.3))
+    is_over_mean_feature = is_over_mean_feature.map(boolean2int)
+    is_over_mean_feature_1 = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 0.5))
+    is_over_mean_feature_1 = is_over_mean_feature_1.map(boolean2int)
+    is_over_mean_feature_2 = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 1))
+    is_over_mean_feature_2 = is_over_mean_feature_2.map(boolean2int)
+    is_over_mean_feature_3 = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 2))
+    is_over_mean_feature_3 = is_over_mean_feature_3.map(boolean2int)
+    is_over_mean_feature_4 = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 5))
+    is_over_mean_feature_4 = is_over_mean_feature_4.map(boolean2int)
+    is_over_mean_feature_5 = raw_df['value'].apply(lambda x: ((abs(x - mean) / mean) > 0.75))
+    is_over_mean_feature_5 = is_over_mean_feature_5.map(boolean2int)
+    diff_timestamp_feature = raw_df['timestamp'].diff()
+    diff_timestamp_feature = pd.DataFrame(diff_timestamp_feature).bfill()
+
+    manual_features = pd.DataFrame()
+    manual_features['raw_value'] = raw_value
+    manual_features['is_zero_feature'] = is_zero_feature
+    manual_features['is_near_zero_feature'] = is_near_zero_feature
+    manual_features['diff_feature'] = diff_feature
+    manual_features['diff_minus_mean_feature'] = diff_minus_mean_feature
+    manual_features['value_minus_mean_feature'] = value_minus_mean_feature
+    manual_features['abs_value_minus_mean_feature'] = abs_value_minus_mean_feature
+    manual_features['is_over_mean_feature'] = is_over_mean_feature
+    manual_features['diff_timestamp_feature'] = diff_timestamp_feature
+    manual_features['is_over_mean_feature_1'] = is_over_mean_feature_1
+    manual_features['is_over_mean_feature_2'] = is_over_mean_feature_2
+    manual_features['is_over_mean_feature_3'] = is_over_mean_feature_3
+    manual_features['is_over_mean_feature_4'] = is_over_mean_feature_4
+    manual_features['is_over_mean_feature_5'] = is_over_mean_feature_5
+    manual_features = manual_features.reset_index(drop=True)
+    return manual_features
+
+
+
+
+window = 8
+# score_threshold = 0.997
+# KPI_ID_name = '76f4550c43334374' 8a20c229e9860d0c
+train_data_path = 'resources/train.csv'
+test_data_path = 'resources/test.csv'
+augment_data_path = 'resources/augment_data/'
+test_augment_data_path = 'resources/test_augment_data/'
+full_result_path = 'resources/result/prediction.csv'
+xgb_result_path = 'resources/result_xgb/merge_man_xgb_prediction.csv'
+output_path = 'resources/label_prediction_plot'
+manual_features_path = 'resources/manual_feature'
+test_data_raw = pd.read_csv(test_data_path)
+train_data_raw = pd.read_csv(train_data_path)
+
+KPI_LIST, KPI_ID = SplitKPIList(train_data_raw)
+KPI_LIST_test, KPI_ID_test = SplitKPIList(test_data_raw)
+
+# print(KPI_LIST)
+# print('KPI_ID:', KPI_ID)
+# KPI_ID:
+# ['71595dd7171f4540',
+# '88cf3a776ba00e7c',
+# 'affb01ca2b4f0b45',
+# '769894baefea4e9e',
+# '02e99bd4f6cfb33f',
+# '54e8a140f6237526',
+# 'b3b2e6d1a791d63a',
+# '07927a9a18fa19ae',
+# 'a40b1df87e3f1c87',
+# 'c58bfcbacb2822d1',
+# '1c35dbf57f55f5e4',
+# 'cff6d3c01e6a6bfa',
+# '7c189dd36f048a6c',
+# '76f4550c43334374',
+# '18fbb1d5a5dc099d',
+# '9bd90500bfd11edb',
+# '8a20c229e9860d0c',
+# 'e0770391decc44ce',
+# '09513ae3e75778a3',
+# '046ec29ddf80d62e',
+# '8c892e5525f3e491',
+# '8bef9af9a922e0b3',
+# '40e25005ff8992bd',
+# 'da403e4e3f87c9e0',
+# 'a5bf5d65261d859a',
+# '9ee5879409dccef9']
+
+KPI_ID_e = ['07927a9a18fa19ae', '76f4550c43334374']
+
+for KPI_ID_name in KPI_ID:
+
+    print("current KPI ID:", KPI_ID_name)
+
+    index = KPI_ID.index(KPI_ID_name)
+    print("current KPI ID index:", index, "/", len(KPI_ID))
+    filtered_feature_name_list = []
+
+    y = KPI_LIST[index].pop('label')
+    y_shift = y
+    y = y.values[window:]
+    y = pd.Series(y)
+
+    # print("y length:", len(y_shift))
+    # y_shift = y_shift.values[int(window/2):len(y_shift)-int(window/2)]
+    y_shift = y_shift.values
+    # print("y_shift length:", len(y_shift))
+    y_shift = pd.Series(y_shift)
+
+    # y = y.reset_index(drop=True)
+    # print(y)
+    # calculate manual features
+
+    manual_feature_df = get_manual_feature(KPI_LIST[index])
+    lstm_diff_train, lstm_diff_test = get_lstm_diff(KPI_ID_name)
+    manual_feature_df['lstm_diff'] = lstm_diff_train
+
+    manual_feature_df.drop(range(window), inplace=True)
+    manual_feature_df = manual_feature_df.reset_index(drop=True)
+    ts_KPI_ID = KPI_LIST[index].pop('KPI ID')
+    print("ts_KPI_ID:\n", ts_KPI_ID.iloc[[0]])
+
+    X_train_df = pd.read_csv("resources/ts_feature_" + "window_" + str(window) + "_KPI_" + KPI_ID_name + ".csv")
+
+    del X_train_df['id']
+
+    X_train_df = pd.concat([X_train_df, manual_feature_df], axis=1)
+
+    X_train = X_train_df.values
+#    pd.set_option('mode.use_inf_as_na', True)
+    # sc = MinMaxScaler()
+    # ts_X_train_df = pd.read_csv("resources/ts_feature_"+"window_"+str(window)+"_KPI_"+KPI_ID_name+".csv")
+
+    # del ts_X_train_df['id']
+    # ts_X_train = ts_X_train_df.values
+
+    y_train_shift = y_shift.values
+
+    # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train_shift, test_size=0.33, stratify=y_train_shift, random_state=seed)
+
+    y_train = y_train_shift
+
+    print("X_train shape:")
+    print(X_train.shape)
+    print("y_train length:")
+    print(len(y_train))
+
+    # X_train_ = X_train
+    # y_train_ = y_train
+
+    # y_train_shift_ = y_train_shift
+    print("len(X_train), len(y_train):")
+    print(len(X_train), len(y_train))
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    # dtest = xgb.DMatrix(X_test)
+
+    params = {
+        'objective': 'binary:logistic',
+        'max_depth': 5,
+        'silent': 1,
+        'eta': 5,
+        'learning_rate': 0.19,
+        'n_estimators': 100000
+    }
+
+    num_rounds = 50
+
+    train_labels = dtrain.get_label()
+
+    ratio = float(np.sum(train_labels == 0)) / np.sum(train_labels == 1)
+    # print("ratio:", ratio)
+    params['scale_pos_weight'] = ratio
+
+    bst = xgb.train(params, dtrain, num_rounds)
+    # y_test_preds = (bst.predict(dtest) > 0.5).astype('int')
+    # print(confusion_matrix(y_test, y_test_preds))
+    #
+    # print('Accuracy: {0:.4f}'.format(accuracy_score(y_test, y_test_preds)))
+    # print('Precision: {0:.4f}'.format(precision_score(y_test, y_test_preds)))
+    # print('Recall: {0:.4f}'.format(recall_score(y_test, y_test_preds)))
+    # print('f1: {0:.4f}'.format(f1_score(y_test, y_test_preds)))
+    # print('roc_auc_score: {0:.4f}'.format(roc_auc_score(y_test, y_test_preds)))
+
+
+
+    # prediction ##################################----------------------------
+
+    index = KPI_ID_test.index(KPI_ID_name)
+
+    test_manual_feature = get_manual_feature(KPI_LIST_test[index])
+    test_manual_feature['lstm_diff'] = lstm_diff_test
+    test_manual_feature.drop(range(window), inplace=True)
+    test_manual_feature = test_manual_feature.reset_index(drop=True)
+
+    ts_KPI_ID_test = KPI_LIST_test[index].pop('KPI ID')
+    ts_timestamp = KPI_LIST_test[index]['timestamp']
+    print("ts_KPI_ID_test:\n", ts_KPI_ID_test.iloc[[0]])
+    X_train_df = pd.read_csv(
+        "resources/test_feature/ts_feature_" + "window_" + str(window) + "_KPI_" + KPI_ID_name + ".csv")
+    del X_train_df['id']
+    X_train_df = pd.concat([X_train_df, test_manual_feature], axis=1)
+    X_train = X_train_df.values
+
+
+    print("X_train.shape:", X_train.shape)
+
+    dtest = xgb.DMatrix(X_train)
+    y_test_preds = (bst.predict(dtest) > 0.5).astype('int')
+
+    xgb_predict = y_test_preds
+    # xgb_predict = padding_shift_y(y_test_preds, window)
+    # shift_predict = padding_shift_y(cl_shift.predict(X_train), window)
+    # split_predict = padding_y(cl_split.predict(X_train), window)
+    # full_ts_result = get_result(ts_KPI_ID_test, ts_timestamp, full_predict)
+    xgb_ts_result = get_result(ts_KPI_ID_test, ts_timestamp, xgb_predict)
+    print("xgb_ts_result:", ts_KPI_ID_test.iloc[[0]])
+
+    save(result_path=xgb_result_path, ts_result=xgb_ts_result)
+
+print("finish !!!")
+
+
